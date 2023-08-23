526cb193d620ac1861fe035cff4174070495c3c2
Jaspreet Singh <123469919+Jass6268@users.noreply.github.com>
2023-08-23 11:11:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hbtestingbot:123@cluster0.tgvrnye.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Skfilter:skfilter@skfilter.kmk96t1.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "skfilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
